82bc2bfa1b58ca3c09e2fb13cafb5c2ff09ef935
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 11:53:28 +0400

    add attributes into property class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Property.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Property.java
@@ -1,4 +1,26 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "property")
 public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long property_id;
+    String address;
+    String type;
+    Double price;
+    Integer size;
+    Integer number_of_bedrooms;
+    Integer number_of_bathrooms;
+    String description;
+
 }
