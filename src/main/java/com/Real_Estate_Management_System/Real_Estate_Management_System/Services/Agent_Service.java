1a829b99ae5600324972f72b24b160587425d46e
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:30:57 +0400

    add code into agent service class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Agent_Service.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Agent_Service.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Services;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Agent;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Repositories.Agent_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class Agent_Service {
+    @Autowired
+    Agent_Repository agent_repository;
+    public List<Agent> getAllAgents() {
+
+        return agent_repository.findAll();
+    }
+
+    public Agent getAgentById(Long id) {
+
+        return agent_repository.findById(id).get();
+    }
 }
