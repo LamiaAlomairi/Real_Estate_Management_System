1737f077bc368ac7f9c4f6172a463ab2278822d7
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 14:21:59 +0400

    add code into Listing_Service class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Listing_Service.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Listing_Service.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Services;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Listing;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Repositories.Listing_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class Listing_Service {
+    @Autowired
+    Listing_Repository listing_repository;
+    public List<Listing> getAllListings() {
+
+        return listing_repository.findAll();
+    }
+
+    public Listing getListingById(Long id) {
+
+        return listing_repository.findById(id).get();
+    }
 }
