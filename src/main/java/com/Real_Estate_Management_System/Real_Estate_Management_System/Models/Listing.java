b4ce443a6389f05948c14c103d6921e3cdfae419
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:04:21 +0400

    add attributes into listing class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Listing.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Listing.java
@@ -1,4 +1,21 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "listing")
 public class Listing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long listing_id;
+    Long price;
+    String date_listed;
+
 }
