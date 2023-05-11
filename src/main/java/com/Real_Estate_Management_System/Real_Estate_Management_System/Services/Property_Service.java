b5ddf50f1f0f6301cc9baefb8df787f02f6e7cee
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 14:39:39 +0400

    add code into Property_Service class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Property_Service.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Property_Service.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Services;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Property;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Repositories.Property_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class Property_Service {
+    @Autowired
+    Property_Repository property_repository;
+    public List<Property> getAllPropertys() {
+
+        return property_repository.findAll();
+    }
+
+    public Property getPropertyById(Long id) {
+
+        return property_repository.findById(id).get();
+    }
 }
