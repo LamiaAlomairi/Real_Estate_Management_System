ed92861000da5d586a942f8dd24e0b3d839ba900
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:32:26 +0400

    add code into Appraisal_Service class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Appraisal_Service.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Appraisal_Service.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Services;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Appraisal;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Repositories.Appraisal_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class Appraisal_Service {
+    @Autowired
+    Appraisal_Repository appraisal_repository;
+    public List<Appraisal> getAllAppraisals() {
+
+        return appraisal_repository.findAll();
+    }
+
+    public Appraisal getAppraisalById(Long id) {
+
+        return appraisal_repository.findById(id).get();
+    }
 }
