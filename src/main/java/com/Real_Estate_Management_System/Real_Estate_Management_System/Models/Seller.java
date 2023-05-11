c7c64fe4be921afab479a0c6375e53e89c0fc991
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:00:12 +0400

    add attributes into seller class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Seller.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Seller.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "seller")
 public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long seller_id;
+    String name;
+    String email;
+    Long phone;
+    Double asking_price;
+
 }
