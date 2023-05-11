ab8a17614f131e1bcdc9e9506709ac15407fce21
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 11:58:01 +0400

    add attributes into buyer class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Buyer.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Buyer.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "buyer")
 public class Buyer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long buyer_id;
+    String name;
+    String email;
+    Long phone;
+    String budget;
+
 }
