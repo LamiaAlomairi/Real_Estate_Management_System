b83f552b8db3fc0179497505479ed8aa67a27c3a
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 11:27:25 +0400

    create project and packages

---
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/RealEstateManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Real_Estate_Management_System.Real_Estate_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Real_Estate_Management_System/Real_Estate_Management_System/RealEstateManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Real_Estate_Management_System.Real_Estate_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
