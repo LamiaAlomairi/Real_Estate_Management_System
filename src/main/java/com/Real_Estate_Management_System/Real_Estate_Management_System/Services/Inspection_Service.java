0ee62bb1dd169c2f906f584dfa2bd65c1c86d47e
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 14:20:24 +0400

    add code into Inspection_Service class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Inspection_Service.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Inspection_Service.java
@@ -1,5 +1,7 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Services;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Inspection;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Repositories.Inspection_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +9,15 @@ import java.util.List;
 
 @Service
 public class Inspection_Service {
+    @Autowired
+    Inspection_Repository inspection_repository;
+    public List<Inspection> getAllInspections() {
 
+        return inspection_repository.findAll();
+    }
+
+    public Inspection getInspectionById(Long id) {
+
+        return inspection_repository.findById(id).get();
+    }
 }
