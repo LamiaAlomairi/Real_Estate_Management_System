d63b5f5a9902589aaef6cd1c22927122b40e33a5
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 14:23:59 +0400

    add code into Payment_Service class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Payment_Service.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Services/Payment_Service.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Services;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Payment;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Repositories.Payment_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class Payment_Service {
+    @Autowired
+    Payment_Repository payment_repository;
+    public List<Payment> getAllPayments() {
+
+        return payment_repository.findAll();
+    }
+
+    public Payment getPaymentById(Long id) {
+
+        return payment_repository.findById(id).get();
+    }
 }
