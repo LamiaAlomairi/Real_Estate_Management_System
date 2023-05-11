ebbf455aeb2dcc12418329aaffef31f89e8471ae
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:10:27 +0400

    add attributes into contract class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Contract.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Contract.java
@@ -1,4 +1,21 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "contract")
 public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long contract_id;
+    Double price;
+    String date_signed;
+
 }
