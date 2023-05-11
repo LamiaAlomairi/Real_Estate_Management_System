4c2907850a695f4129c3bffe2bb90f19b22a4aef
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 14:46:26 +0400

    add code to Appraisal_Controller class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Controllers/Appraisal_Controller.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Controllers/Appraisal_Controller.java
@@ -1,4 +1,25 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Controllers;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Appraisal;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Services.Appraisal_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping(value = "appraisal")
 public class Appraisal_Controller {
+    //    http://localhost:8080/appraisal/getAll
+    @Autowired
+    Appraisal_Service appraisal_service;
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Appraisal> getAllAppraisals() {
+
+        return appraisal_service.getAllAppraisals();
+    }
+    @GetMapping(value = "getById")
+    public Appraisal getAppraisalById(@RequestParam Long id) {
+
+        return appraisal_service.getAppraisalById(id);
+    }
 }
