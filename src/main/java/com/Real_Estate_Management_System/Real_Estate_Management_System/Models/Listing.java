0fb79733141b68e57c3065f05bbbef4779d3373a
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 15:20:22 +0400

    add a relationships in listing class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Listing.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Listing.java
@@ -1,10 +1,12 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +20,23 @@ public class Listing {
     Long price;
     String date_listed;
 
+    @ManyToOne
+    @JoinColumn(name = "property_id", referencedColumnName = "property_id")
+    Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "agent_id", referencedColumnName = "agent_id")
+    Agent agent;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id", referencedColumnName = "seller_id")
+    Seller seller;
+
+    @OneToMany(mappedBy = "listing")
+    @JsonIgnore
+    private List<Inspection> inspections;
+
+    @OneToMany(mappedBy = "listing")
+    @JsonIgnore
+    private List<Contract> contracts;
 }
