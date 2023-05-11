a8e3d50612c7f9cd2425f68b92b2729889a546b8
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:13:06 +0400

    add attributes into inspection class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Inspection.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Inspection.java
@@ -1,4 +1,21 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "inspection")
 public class Inspection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long inspection_id;
+    String date_inspected;
+    String report;
+
 }
