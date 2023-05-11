dd8d2bbc91a32e791d0589f5d3b889e2f419d24f
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 11:55:28 +0400

    add attributes into agent class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Agent.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Agent.java
@@ -1,4 +1,23 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "agent")
 public class Agent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long agent_id;
+    String name;
+    String email;
+    Long phone;
+    Long license_number;
+
 }
