0ce89dd7736ed290965dcc123f7a50aadc8d7ca2
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 14:48:20 +0400

    add code to Buyer_Controller class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Controllers/Buyer_Controller.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Controllers/Buyer_Controller.java
@@ -1,4 +1,26 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Controllers;
 
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Models.Buyer;
+import com.Real_Estate_Management_System.Real_Estate_Management_System.Services.Buyer_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "buyer")
 public class Buyer_Controller {
+    //    http://localhost:8080/buyer/getAll
+    @Autowired
+    Buyer_Service buyer_service;
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Buyer> getAllBuyers() {
+
+        return buyer_service.getAllBuyers();
+    }
+    @GetMapping(value = "getById")
+    public Buyer getBuyerById(@RequestParam Long id) {
+
+        return buyer_service.getBuyerById(id);
+    }
 }
