80f271dd864020f538f6ee3e12224d49863891c7
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 15:06:45 +0400

    add a relationships in property class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Property.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Property.java
@@ -1,10 +1,12 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,4 +25,23 @@ public class Property {
     Integer number_of_bathrooms;
     String description;
 
+    @OneToMany(mappedBy = "property")
+    @JsonIgnore
+    private List<Listing> listings;
+
+    @OneToMany(mappedBy = "property")
+    @JsonIgnore
+    private List<Appraisal> appraisals;
+
+    @OneToMany(mappedBy = "property")
+    @JsonIgnore
+    private List<Inspection> inspections;
+
+    @OneToMany(mappedBy = "property")
+    @JsonIgnore
+    private List<Contract> contracts;
+
+    @OneToMany(mappedBy = "property")
+    @JsonIgnore
+    private List<Payment> payments;
 }
