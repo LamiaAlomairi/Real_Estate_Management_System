d88d380e4712505493f8cfb5fe0ea06a70bb82ff
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:16:38 +0400

    add attributes into appraisal class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Appraisal.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Appraisal.java
@@ -1,4 +1,21 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "appraisal")
 public class Appraisal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long appraisal_id;
+    String date_appraised;
+    Integer value;
+
 }
