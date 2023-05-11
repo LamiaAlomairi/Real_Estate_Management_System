619d8852a093fedf9e8d41dca2a8b47c7f4b81da
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 12:25:20 +0400

    add attributes into payment class

--- src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Payment.java
+++ src/main/java/com/Real_Estate_Management_System/Real_Estate_Management_System/Models/Payment.java
@@ -1,4 +1,21 @@
 package com.Real_Estate_Management_System.Real_Estate_Management_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "payment")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long payment_id;
+    Double amount;
+    String date_paid;
+
 }
